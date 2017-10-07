916e42fb7b5c0db80d021b2ce3557f04b65754bc
Michał G <m.k.gozdzikiewicz@gmail.com>
2017-10-07 10:38:39 +0200

    Create word class

---
+++ src/model/Capital.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Capital {
+
+    private String name;
+    private String dash;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDash() {
+        return dash;
+    }
+
+    public void setDash(String dash) {
+        this.dash = dash;
+    }
+}
